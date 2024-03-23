3b126202ea28effd97e73217aa93b8d6f0f04f3e
bbabatop@gmail.com <Jbaba.bbabatop@gmail.com>
2024-03-23 15:15:32 -0400

    1st commit

---
+++ milo/src/main/java/com/jiggy/milo/App.java
@@ -0,0 +1,13 @@
+package com.jiggy.milo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
