014a42eccebc05d06c56807a6db0ab789f0dfee6
bbabatop@gmail.com <Jbaba.bbabatop@gmail.com>
2024-03-27 21:34:18 -0400

    maven_git_commit

---
+++ soccerproject/src/main/java/com/baller/soccerproject/App.java
@@ -0,0 +1,13 @@
+package com.baller.soccerproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
